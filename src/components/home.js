2859f13d180f69b5e2756129798f2c2626d1a8c0
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-04-20 21:12:50 -0500

    cambios en stylo y errores de firebase a español

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-shadow */
 import { getAuth, signOut } from 'firebase/auth';
 import {
-  // eslint-disable-next-line max-len
   collection, query, getDocs, addDoc, deleteDoc, doc, updateDoc,
 } from 'firebase/firestore';
 import { initFirebase } from '../helpers/firebase';
@@ -9,8 +10,6 @@ import { navigateTo } from '../router';
 export const Home = () => {
   document.body.classList.add('home-background');
   document.body.classList.remove('others-background');
-  const auth = getAuth();
-  const user = auth.currentUser;
   const div = document.createElement('div');
   div.className = 'muro';
   div.innerHTML = `
@@ -24,7 +23,7 @@ export const Home = () => {
     <form id="post-form" class="post-form">
     <p>¿Cuál ha sido tu destino de viaje favorito hasta ahora y por qué lo recomendarías?</p>
     <div class="contenedor-img-text">
-    <textarea id="post-content" placeholder="Cuéntanos tus aventuras......" required></textarea>
+    <textarea id="post-content" placeholder="Cuéntanos tus aventuras......" ></textarea>
     </div>
     <div class="contenedor-btn-publicar"><button type="submit" class='btn-registros'>Publicar</button></div>
     </form>
@@ -35,7 +34,6 @@ export const Home = () => {
   const { db } = initFirebase(); // obtener la referencia al objeto db
 
   div.querySelector('.cerrar-sesion').addEventListener('click', (e) => {
-    // eslint-disable-next-line no-shadow
     const auth = getAuth();
     signOut(auth)
       .then(() => {
@@ -43,7 +41,7 @@ export const Home = () => {
         alert('Sesión cerrada con éxito');
       })
       .catch((error) => {
-        console.error(error);
+        alert(error);
       });
   });
 
@@ -59,35 +57,8 @@ export const Home = () => {
       </header>
       <p class="texto-descripcion">${publicacion.descripcion}</p>
       `;
-      // eslint-disable-next-line no-shadow
-
-      // eslint-disable-next-line no-shadow
       const auth = getAuth();
       const currentUser = auth.currentUser;
-
-      // Botón de "Me gusta"
-      // const likeButton = document.createElement('div');
-      // const likeImg = document.createElement('img');
-      // likeImg.setAttribute('class', 'like');
-      // likeImg.setAttribute('src', 'assets/likeVacio.png');
-      // likeButton.appendChild(likeImg);
-      // likeImg.addEventListener('click', async () => {
-      //   try {
-      //     // eslint-disable-next-line no-use-before-define
-      //     await darMeGusta(publicacion.id, currentUser.uid);
-      //     // Actualizar el número de likes en la publicación
-      //     publicacion.likes += 1;
-      //     const likesContainer = postDiv.querySelector('.likes-container');
-      // eslint-disable-next-line max-len
-      //     likesContainer.innerHTML = `${publicacion.likes} ${publicacion.likes === 1 ? 'me gusta' : 'me gustan'}`;
-      //     // Cambiar el icono de "Me gusta"
-      //     likeImg.setAttribute('src', 'assets/likeLleno.png');
-      //   } catch (e) {
-      //     console.error('Error al dar "Me gusta":', e);
-      //   }
-      // });
-      /* si el usuario esta logeado y su uid es igual a la de la publicacion entonces
-      puede editar y elimar su post */
 
       if (currentUser && currentUser.uid === publicacion.uid) {
         const contenedorCajaEditar = document.createElement('div');
@@ -117,7 +88,6 @@ export const Home = () => {
             }
           }
         });
-        // añadimos un escuchador de eventos para cuando demos click en la imagen poder editar post
 
         editContent.addEventListener('click', () => {
           // Verificar si ya hay un formulario de edición
@@ -131,12 +101,11 @@ export const Home = () => {
             const form = document.createElement('div');
             form.className = 'form-edicion';
             form.innerHTML = `
-      <input type="text" id="descripcion" class="item-edit" placeholder="Editar descripción:" name="descripcion" value="${publicacion.descripcion}">
-      <div>
-        <button type="button" id="guardar">Guardar</button>
-        <button type="button" id="cancelar">Cancelar</button>
-      </div>
-    `;
+            <input type="text" id="descripcion" class="item-edit" placeholder="Editar descripción:" name="descripcion" value="${publicacion.descripcion}">
+            <div class="contenedor-btn-edit">
+            <button type="button" id="guardar" class="btn-guardar">Guardar</button>
+            <button type="button" id="cancelar" class="btn-cancelar">Cancelar</button>
+            </div>  `;
             // Agregar el formulario al DOM
             contenedorCajaEditar.appendChild(form);
             // Agregar controlador de eventos al botón "Cancelar"
@@ -151,18 +120,24 @@ export const Home = () => {
             guardarBtn.addEventListener('click', async () => {
               const nuevoContenido = document.getElementById('descripcion').value;
               if (nuevoContenido !== null && nuevoContenido !== '') {
-                try {
-                  await editarPublicacion(publicacion.id, nuevoContenido);
-                  // Actualizar la descripción en la página y ocultar el formulario
-                  publicacion.descripcion = nuevoContenido;
-                  alert('guardado con exito');
-                  document.getElementById('descripcion-publicacion').textContent = nuevoContenido;
-                  form.style.display = 'none';
-                } catch (e) {
-                  console.log(e);
+                const contenidoRegex = /[a-zA-Z]/;
+                if (contenidoRegex.test(nuevoContenido)) {
+                  try {
+                    await editarPublicacion(publicacion.id, nuevoContenido);
+                    // Actualizar la descripción en la página y ocultar el formulario
+                    publicacion.descripcion = nuevoContenido;
+                    alert('Guardado con éxito');
+                    document.getElementById('descripcion-publicacion').textContent = nuevoContenido;
+                    form.style.display = 'none';
+                  } catch (e) {
+                    console.log(e);
+                  }
+                // eslint-disable-next-line keyword-spacing
+                } else {
+                  alert('Error, el contenido debe contener letras');
                 }
               } else if (nuevoContenido === '') {
-                alert('Error , el contenido no puede estar vacio ');
+                alert('Error, el contenido no puede estar vacío');
               }
             });
           }
@@ -174,25 +149,6 @@ export const Home = () => {
       postList.appendChild(postDiv);
     });
   };
-
-  // funcion para dar me gusta
-  // const darMeGusta = async (publicacionId, uid) => {
-  //   const docRef = doc(db, 'publicaciones', publicacionId);
-  //   const publicacion = await getDoc(docRef);
-  //   const usuariosQueDieronLike = publicacion.data().usuariosQueDieronLike;
-  //   if (!usuariosQueDieronLike.includes(uid)) {
-  //     await updateDoc(docRef, {
-  //       likes: increment(1),
-  //       usuariosQueDieronLike: arrayUnion(uid),
-  //     });
-  //   } else {
-  //     await updateDoc(docRef, {
-  //       likes: increment(-1),
-  //       usuariosQueDieronLike: arrayRemove(uid),
-  //     });
-  //   }
-  // };
-
   /* La función editarPublicacion es una función asíncrona que toma dos argumentos: publicacionId,
    que es el ID de la publicación a editar, y nuevoContenido, que es la nueva descripción que se
    desea establecer para la publicación. */
@@ -204,14 +160,12 @@ export const Home = () => {
     const publicaciones = await obtenerPublicaciones();
     mostrarPublicaciones(publicaciones);
   };
-
-  /* funcion para obtener las publicaciones almacenadas en una colección Firestore de
+  /* obtener las publicaciones almacenadas en una colección Firestore de
   Firebase y devolverlas como un array de objetos que contienen el ID del documento y sus datos. */
   const obtenerPublicaciones = async () => {
     const q = query(collection(db, 'publicaciones'));
     const querySnapshot = await getDocs(q);
     const publicaciones = [];
-    // eslint-disable-next-line no-shadow
     querySnapshot.forEach((doc) => {
       publicaciones.push({ id: doc.id, ...doc.data() });
     });
@@ -228,6 +182,11 @@ export const Home = () => {
   postForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const postContent = postForm.querySelector('#post-content').value;
+    if (postContent === '') {
+      alert('La publicacion no puede estar vacia, por favor inserte palabras');
+    };
+    const auth = getAuth();
+    const user = auth.currentUser;
     const post = {
       autorPhotoURL: user.photoURL || `https://ui-avatars.com/api/?name=${user.email.split('@')[0]}&size=96&background=007bff&color=fff&rounded=true`,
       descripcion: postContent,
@@ -235,8 +194,6 @@ export const Home = () => {
       fecha_creacion: new Date(),
       uid: user.uid,
     };
-    /* try y catch el código puede manejar de manera efectiva cualquier excepción que pueda surgir
-    y evitar que la aplicación se bloquee o cierre debido a un error no controlado. */
     try {
     // agregar la publicación a la base de datos
       const docRef = await addDoc(collection(db, 'publicaciones'), post);
--- src/components/login.js
+++ src/components/login.js
@@ -23,9 +23,9 @@ export const Login = () => {
   </div>
   </button>
   <p>o</p>
-  <input type="email" id="username" name="username" placeholder="Correo electrónico " required>
+  <input type="email" id="username" name="username" placeholder="Correo electrónico ">
   <div style="height: 16px;"></div>
-  <input type="password" id="password" name="password" placeholder="Contraseña" required>
+  <input type="password" id="password" name="password" placeholder="Contraseña" >
   <div style="height: 32px;"></div>
   <button class="btn-registros">Iniciar sesión</button>
   <div style="height: 16px;"></div>
@@ -59,18 +59,34 @@ export const Login = () => {
     div.querySelector('.modal').style.display = 'none';
   });
 
-  // Add event listeners to the login component
-
   div.querySelector('#loginForm').addEventListener('submit', (e) => {
     e.preventDefault();
-    const username = div.querySelector('#username').value;
+    const email = div.querySelector('#username').value;
     const password = div.querySelector('#password').value;
-    signInWithPassword(username, password).then(
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+      openModal('Por favor, introduce un correo electrónico válido.');
+      return;
+    }
+    if (password.length < 6) {
+      openModal('Por favor, introduce una contraseña de al menos 6 caracteres.');
+      return;
+    }
+    signInWithPassword(email, password).then(
       (useCredential) => {
         navigateTo('/home');
       },
       (error) => {
-        openModal(error.message);
+        const errorCode = error.code;
+        let errorMessage;
+        if (errorCode === 'auth/wrong-password') {
+          errorMessage = 'La contraseña es incorrecta. Por favor, inténtalo de nuevo.';
+        } else if (errorCode === 'auth/user-not-found') {
+          errorMessage = 'No se ha encontrado una cuenta con este correo electrónico. Por favor, regístrate primero.';
+        } else {
+          errorMessage = 'Ha ocurrido un error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.';
+        }
+        openModal(errorMessage);
       },
     );
   });
@@ -82,10 +98,54 @@ export const Login = () => {
         navigateTo('/home');
       },
       (error) => {
-        openModal(error.message);
+        switch (error.code) {
+          case 'auth/popup-closed-by-user':
+            openModal('El inicio de sesión se ha cancelado.');
+            break;
+          case 'auth/cancelled-popup-request':
+            openModal('El inicio de sesión se ha cancelado.');
+            break;
+          case 'auth/web-storage-unsupported':
+            openModal('El navegador no soporta el almacenamiento web o el usuario ha desactivado este soporte.');
+            break;
+          case 'auth/operation-not-supported-in-this-environment':
+            openModal('Esta operación no es compatible en el entorno actual.');
+            break;
+          case 'auth/auth-domain-config-required':
+            openModal('La configuración del dominio de autenticación es obligatoria.');
+            break;
+          case 'auth/credential-already-in-use':
+            openModal('Esta credencial ya está en uso.');
+            break;
+          case 'auth/user-disabled':
+            openModal('La cuenta de usuario ha sido deshabilitada.');
+            break;
+          case 'auth/user-token-expired':
+            openModal('El token de usuario ha expirado.');
+            break;
+          case 'auth/invalid-email':
+            openModal('La dirección de correo electrónico proporcionada no es válida.');
+            break;
+          case 'auth/user-not-found':
+            openModal('No se encontró una cuenta con la dirección de correo electrónico proporcionada.');
+            break;
+          case 'auth/wrong-password':
+            openModal('La contraseña proporcionada no es válida.');
+            break;
+          case 'auth/popup-blocked':
+            openModal('El inicio de sesión emergente ha sido bloqueado por el navegador.');
+            break;
+          case 'auth/network-request-failed':
+            openModal('Ha ocurrido un error de red, por favor comprueba tu conexión.');
+            break;
+          default:
+            openModal('Ha ocurrido un error desconocido, por favor intenta de nuevo más tarde.');
+            break;
+        }
       },
     );
   });
+
   div.querySelector('.signup-btn').addEventListener('click', (e) => {
     e.preventDefault();
     navigateTo('/register');
